fc7411dcd61364b1c9be93f482e305c1f70ca035
sean1rose <sean1rose@gmail.com>
2016-05-03 00:29:21 -0700

    Set up react router

---
+++ app/config/routes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Router, Route, hashHistory } from 'react-router'
+import { MainContainer } from '../containers'
+
+const routes = (
+	<Router history={hashHistory}>
+		<Router path='/' component={MainContainer} />
+	</Router>
+)
+
+export default routes
