bc0a71866ed03b81192393051799afb891e91ce4
sean1rose <sean1rose@gmail.com>
2016-05-12 01:51:17 -0700

    Set up mapDispatchToProps convenience method

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react'
 import { Authenticate } from 'components'
 import auth from 'helpers/auth'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 // import { fetchingUserSuccess, authUser, unauthUser, etc...} from 'redux/modules/users'
 // ===
 import * as userActionCreators from 'redux/modules/users'
@@ -13,19 +14,23 @@ const AuthenticateContainer = React.createClass({
   propTypes: {
     isFetching: PropTypes.bool.isRequired,
     error: PropTypes.string.isRequired,
+    authUser: PropTypes.func.isRequired,
+    fetchingUser: PropTypes.func.isRequired,
+    fetchingUserFailure: PropTypes.func.isRequired,
+    fetchingUserSuccess: PropTypes.func.isRequired,
   },
   handleAuth () {
     // 1. dispatch fetchingUser action creator -> find action in reducer 'FETCHING_USER' -> that will modify 'isFetching' state to true
-    this.props.dispatch(userActionCreators.fetchingUser())
+    this.props.fetchingUser()
     // so now state prop 'isFetching' is set to 'true'
     auth().then((user) => {
       // want to inform redux store that we have an authenticated user...
       console.log('Authed user', user)
       // 2. dispatch other actions upon success -> updates state by creating new authed user
-      this.props.dispatch(userActionCreators.fetchingUserSuccess(user.uid, user, Date.now()))
-      this.props.dispatch(userActionCreators.authUser(user.uid))
+      this.props.fetchingUserSuccess(user.uid, user, Date.now())
+      this.props.authUser(user.uid)
     })
-    .catch((error) => this.props.dispatch(userActionCreators.fetchingUserFailure(error)))
+    .catch((error) => this.props.fetchingUserFailure(error))
     // SUMMARY: to update state -> dispatch invocation of action creators, which call reducer function via type -> return brand new state
   },
   render () {
@@ -47,8 +52,20 @@ function mapStateToProps (state) {
   }
 }
 
+// convenience method which allows us to bind dispatch to obj that has action creators on it
+function mapDispatchToProps (dispatch) {
+  // pass an obj whos values are action creators
+  return bindActionCreators(userActionCreators , dispatch)
+}
+
+
 // this connects our container to redux store...
-export default connect(mapStateToProps)(AuthenticateContainer)
+// (1st arg - specifies which part of the state the component needs)
+// 2nd arg - convenience method which allows us to bind dispatch to obj that has action creators on it
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AuthenticateContainer)
 // connect returns a function, and that function that returns accepts AuthenticateContainer as its 1st arg
 
 /*
