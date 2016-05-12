7bfb8230ff208b3348e42f17a5590e301183ccb0
sean1rose <sean1rose@gmail.com>
2016-05-12 00:59:35 -0700

    create store and use provider

--- app/index.js
+++ app/index.js
@@ -1,14 +1,15 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 import routes from './config/routes'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import users from 'redux/modules/users'
 
 const store = createStore(users)
-console.log('store - ', store)
-console.log('store.getState() - ', store.getState())
-
 
 ReactDOM.render(
-	routes,
+  <Provider store={store}>
+    {routes}
+  </Provider>,
 	document.getElementById('app')
 )
